653c3cb693ebd01a43194368d78b86fa6ee8fe8b
williamboomer87 <126490002+williamboomer87@users.noreply.github.com>
2023-03-13 23:55:47 +0530

    update code new redirection

--- server/demo.js
+++ server/demo.js
@@ -1,17 +1,19 @@
-import axios from "axios";
 //const axios = require('axios');
+import axios from "axios";
 require('dotenv').config()
 const mongoose = require('mongoose')
 require('./customFunctions/demoModel')
 const User = mongoose.model('chats')
 const shortid = require('shortid')
+import Toastify from 'toastify-js'
+
 
 exports.handler = async (event, context) => {
   console.log(event);
   const array = event.body.split('&')
   // const firstName = array[0].split('fname=')
   // const lastName = array[1].split('lname=')
-  
+
   const name = array[0].split('name=')
   const email = array[1].split('email=')
   const phone = array[2].split('pnumber=')
@@ -43,44 +45,14 @@ exports.handler = async (event, context) => {
   };
 
   axios.request(options).then(function (response) {
-    console.log(response.data);  
+    // console.log(response.data);  
+    Toastify({
+      text: "Data saved successfully. Redirecting to calender page..",
+      duration: 3000
+    }).showToast();
+    //window.location.href = "/calender";
   }).catch(function (error) {
     console.error(error);
   });
 
-  // send to gohighlevel
-  
-
-  try {
-    mongoose.connect(process.env.MONGODB_URI_DEPLOYC, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    })
-
-    const shortIdVariable = shortid.generate()
-
-    const user = await new User({
-      referralId: shortIdVariable,
-      // first_name: a,
-      // last_name: b,
-      name: a,
-      email: b,
-      phone_number: c,
-      company_name: d,
-      // company_size: d,
-    })
-
-    await user.save()
-    mongoose.disconnect()
-    return {
-      statusCode: 200,
-      body: 'Success',
-    }
-  } catch (err) {
-    return {
-      statusCode: 400,
-      body: err,
-    }
-  }
 }
